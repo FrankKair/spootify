37b0940954c9568a90ea049f89af87ea9cb4f0c1
Frank Kair <frankkair@gmail.com>
2019-03-31 20:01:50 +0100

    Adds lastfm test

---
+++ lastfm/lastfm_test.go
@@ -0,0 +1,15 @@
+package lastfm
+
+import (
+	"testing"
+
+	"github.com/FrankKair/spootify/track"
+)
+
+func TestCleanAlbumTitle(t *testing.T) {
+	track := track.New("Justin Timberlake", "The 20/20 Experience", "Pusher Love Girl")
+	url := getLastfmURL(track)
+	if url != "https://www.last.fm/music/Justin+Timberlake/The+20%2F20+Experience/+wiki" {
+		t.Error("getLastfmURL is not working properly")
+	}
+}
