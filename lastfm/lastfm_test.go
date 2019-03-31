4f05e48251fbd8106d089d4229f3b9e7a4558773
Frank Kair <frankkair@gmail.com>
2019-03-31 20:18:13 +0100

    Fixes test name

--- lastfm/lastfm_test.go
+++ lastfm/lastfm_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/FrankKair/spootify/track"
 )
 
-func TestCleanAlbumTitle(t *testing.T) {
+func TestGetLastfmURL(t *testing.T) {
 	track := track.New("Justin Timberlake", "The 20/20 Experience", "Pusher Love Girl")
 	url := getLastfmURL(track)
 	if url != "https://www.last.fm/music/Justin+Timberlake/The+20%2F20+Experience/+wiki" {
